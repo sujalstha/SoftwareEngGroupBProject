b82e507563678e02a2a756eaa1a86191a678b942
sujalshrestha <sujalshrestha2004@gmail.com>
2025-09-24 19:26:13 -0500

    First Commit

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """
-CS 2302
-ID: GORUP B
+
+OU TRIVIA APP
+ID: Group B
 OU TRIVIA APP
 By Sujal, Joel, Devin, Jayce, Mo, Ryan, Abraham
 
 """
-
-# libraries needed for the application
 import tkinter as tk
-
+import sqlite3
 
 # OU crimson and cream
 OU_CRIMSON = "#841617"
 OU_CREAM = "#FDF9D8"
+
 
 class StartScreen:
     def __init__(self, root):
@@ -21,40 +21,57 @@ class StartScreen:
         self.root.geometry("500x500")
         self.root.configure(bg=OU_CREAM)
 
+        # Cannot Resize Windows
+        self.root.resizable(False, False)
+
+        # Main frame
+        self.main_frame = tk.Frame(root, bg=OU_CREAM)
+        self.main_frame.pack(expand=True, padx=20, pady=20)
+
         # Title Text
-        self.title_shadow = tk.Label(root, text="OU Trivia", font=("Arial Black", 36),
-                                     fg="black", bg=OU_CREAM)
-
-        self.title_main = tk.Label(root, text="OU Trivia", font=("Arial Black", 36),
+        self.title_main = tk.Label(self.main_frame, text="OU Trivia", font=("Arial Black", 40),
                                    fg=OU_CRIMSON, bg=OU_CREAM)
-        self.title_main.place(x=150, y=90)
+        self.title_main.pack(pady=(0, 10))
 
-        # subtitles
-        self.subtitle = tk.Label(root, text="Test your OU knowledge!",
+        # Subtitle
+        self.subtitle = tk.Label(self.main_frame, text="Test your OU knowledge!",
                                  font=("Arial", 16, "italic"), bg=OU_CREAM, fg="black")
-        self.subtitle.pack(pady=160)
+        self.subtitle.pack(pady=10)
 
-        # start button
-        self.start_button = tk.Button(root, text="Start Game", font=("Arial", 18, "bold"),
+        # Start Button
+        self.start_button = tk.Button(self.main_frame, text="Start Game", font=("Arial", 18, "bold"),
                                       bg=OU_CRIMSON, fg="black", activebackground="#660000",
                                       activeforeground="white", width=12, height=2,
                                       relief="raised", command=self.start_game)
         self.start_button.pack(pady=20)
 
         # Exit Button
-        self.exit_button = tk.Button(root, text="Exit", font=("Arial", 14),
+        self.exit_button = tk.Button(self.main_frame, text="Exit", font=("Arial", 14),
                                      fg="black", width=10, bg=OU_CRIMSON,
                                      activebackground="#660000",
                                      relief="raised", command=root.quit)
         self.exit_button.pack(pady=10)
 
     def start_game(self):
-        # clear screen for new window
         for widget in self.root.winfo_children():
             widget.destroy()
+        PlayerInfoScreen(self.root)
 
-        tk.Label(self.root, text="Game Starting Soon...", font=("Arial Black", 20),
-                 fg=OU_CRIMSON, bg=OU_CREAM).pack(expand=True)
+
+class PlayerInfoScreen:
+    def __init__(self, root):
+        self.root = root
+        tk.Label(self.root, text="Enter your Name", font=("Arial Black", 20),
+                 fg=OU_CRIMSON, bg=OU_CREAM).place(relx=0.5, rely=0.4, anchor='center')
+
+        self.username_entry = tk.Entry(self.root, width=30, font=("Arial", 14), borderwidth=2, relief="solid")
+        self.username_entry.place(relx=0.5, rely=0.5, anchor='center')
+
+        self.start_button = tk.Button(self.root, text="Start Quiz", font=("Arial", 16, "bold"),
+                                      bg=OU_CRIMSON, fg="black", activebackground="#660000",
+                                      activeforeground="white", width=15, height=2,
+                                      relief="raised")
+        self.start_button.place(relx=0.5, rely=0.6, anchor='center')
 
 
 if __name__ == "__main__":
